fd7fcefded30d2ec1857bf46bc753738dab38f01
fontaine pierre <pierre.ftn64@gmail.com>
2016-10-22 20:32:52 +0200

    some stuff

---
+++ tetris/keyboard.h
@@ -0,0 +1,10 @@
+#ifndef keyboard_h
+#define keyboard_h
+
+void pause();
+void moveRigth();
+void moveLeft();
+void drop();
+void rotate(int angle);
+
+#endif
--- tetris/tetris.h
+++ tetris/tetris.h
@@ -1,6 +1,15 @@
 #ifndef tetris_h
 #define tetris_h
 
+typedef enum piece {
+  S,
+  Z,
+  L,
+  J,
+  T,
+  I
+}piece;
 
+int score();
 
 #endif
