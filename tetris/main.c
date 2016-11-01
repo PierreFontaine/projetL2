8db4323a76de124f8423de99262f176693cc5a3f
fontaine pierre <pierre.ftn64@gmail.com>
2016-11-01 20:09:38 +0100

    reconf du terminal grace à termios.h

--- tetris/main.c
+++ tetris/main.c
@@ -5,15 +5,22 @@
 #include "stdio.h"
 #include "unistd.h"
 #include "pthread.h"
+#include "termios.h"
+
+layout l_jeu;
+piece p_jeu;
+pos p_posInit;
 
 void *thread_1(void *arg){
   printf("Nous sommes dans le thread.\n");
   char touche;
   while(1){
-    touche = getchar();
-    if (touche == 'z') {
-      printf("monter piece \n");
-      sleep(1);
+    touche = fgetc(stdin);
+    if (touche == 'q') {
+      pieceMoveToward(p_jeu,WEST,&p_posInit,l_jeu);
+    }
+    if (touche == 'd') {
+      pieceMoveToward(p_jeu,EST,&p_posInit,l_jeu);
     }
   }
 
@@ -23,10 +30,9 @@ void *thread_1(void *arg){
 }
 
 int main(int argc, char const *argv[]) {
-  layout l_jeu;
-  piece p_jeu;
+  static struct termios oldt, newt;
+
   char key,underPiece;
-  pos p_posInit,p_posUnderPiece;
   figure f_jeu;
   pthread_t thread1;
 
@@ -34,6 +40,18 @@ int main(int argc, char const *argv[]) {
   	perror("pthread_create");
   	return EXIT_FAILURE;
   }
+
+  /**
+  * Changement du comportement du terminal sur le buffer
+  * On veut pouvoir dans le thread capturer le dernier caractère sans attendre le retour chariot
+  * cf man termios
+  *
+  **/
+  tcgetattr(STDIN_FILENO, &oldt);//On place les paramètre du terminal dans une struct termios
+  newt = oldt; //initialisation de newt avec oldt
+  newt.c_lflag &= ~(ICANON); //mise a jour du mode canonique sur newt
+  //TCSANOW change occurs immediately | cf : man tcsetattr
+  tcsetattr(STDIN_FILENO, TCSANOW, &newt); //le terminal prend les paramètre de newt
 
   makeBackGround(l_jeu);
   makeBorder(l_jeu);
@@ -46,6 +64,7 @@ int main(int argc, char const *argv[]) {
 
     p_jeu = selectPiece(); //selection d'une piece au hasard
     f_jeu = makeFigure(p_jeu); //mise en mémoire de cette piece pour accès aux params
+
     clrscr();
     displayPieceAt(p_posInit,l_jeu,p_jeu);//Affichage de la piece en haut
     displayGame(l_jeu); //affichage du jeux
@@ -53,6 +72,7 @@ int main(int argc, char const *argv[]) {
     sleep(1);
     if (gameOver(l_jeu,p_jeu,p_posInit)) {
       printf("game over \n");
+      tcsetattr( STDIN_FILENO, TCSANOW, &oldt);
       return 0;
     }
     while(canMoveToward(p_jeu,SUD,p_posInit,l_jeu) == 1){
