c1ae54a599f40e6db26d3bfef6c8c05c8385279b
Fontaine Pierre <Fontaine Pierre>
2016-11-11 10:31:13 +0000

    run on debian jessie

--- tetris/main.c
+++ tetris/main.c
@@ -6,7 +6,6 @@
 #include "unistd.h"
 //#include "pthread.h"
 #include "termios.h"
-
 #ifdef __APPLE__
 #include "SDL.h"
 #endif
@@ -21,6 +20,8 @@ pos p_posInit;
 
 int main(int argc, char *argv[]) {
   static struct termios oldt, newt;
+
+
 /*
   if(pthread_create(&thread1, NULL, thread_1, NULL) == -1){
   	perror("pthread_create");
@@ -38,6 +39,9 @@ int main(int argc, char *argv[]) {
   newt.c_lflag &= ~(ICANON); //mise a jour du mode canonique sur newt
   //TCSANOW change occurs immediately | cf : man tcsetattr
   tcsetattr(STDIN_FILENO, TCSANOW, &newt); //le terminal prend les paramètre de newt
+  SDL_Init(SDL_INIT_VIDEO);
+
+    SDL_SetVideoMode(640, 480, 32, SDL_HWSURFACE);
   game(l_jeu,&p_jeu,&p_posInit);
   tcsetattr( STDIN_FILENO, TCSANOW, &oldt);
   SDL_Quit();
